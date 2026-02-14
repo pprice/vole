963cec7ee3571a76469ae41df5ca603a232c3633
Phil Price <philprice@gmail.com>
2026-02-14 11:01:27 -0800

    Drive JIT symbol registration from runtime registry (vol-mxw7)

--- src/crates/vole-codegen/src/runtime_registry.rs
+++ src/crates/vole-codegen/src/runtime_registry.rs
@@ -463,6 +463,444 @@ pub fn all_symbols() -> &'static [RuntimeSymbol] {
     RUNTIME_SYMBOLS
 }
 
+/// Runtime symbols that can be linked into JIT modules.
+#[derive(Clone, Copy)]
+pub struct LinkableRuntimeSymbol {
+    pub c_name: &'static str,
+    pub ptr: *const u8,
+}
+
+const LINKABLE_RUNTIME_SYMBOLS: &[LinkableRuntimeSymbol] = &[
+    LinkableRuntimeSymbol {
+        c_name: "rc_inc",
+        ptr: vole_runtime::value::rc_inc as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "rc_dec",
+        ptr: vole_runtime::value::rc_dec as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_string_new",
+        ptr: vole_runtime::string::vole_string_new as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_string_inc",
+        ptr: vole_runtime::string::vole_string_inc as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_string_dec",
+        ptr: vole_runtime::string::vole_string_dec as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_string_len",
+        ptr: vole_runtime::string::vole_string_len as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_string_data",
+        ptr: vole_runtime::string::vole_string_data as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_string_eq",
+        ptr: vole_runtime::string::vole_string_eq as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_string_concat",
+        ptr: vole_runtime::builtins::vole_string_concat as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_println_string",
+        ptr: vole_runtime::builtins::vole_println_string as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_println_i64",
+        ptr: vole_runtime::builtins::vole_println_i64 as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_println_f64",
+        ptr: vole_runtime::builtins::vole_println_f64 as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_println_bool",
+        ptr: vole_runtime::builtins::vole_println_bool as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_print_string",
+        ptr: vole_runtime::builtins::vole_print_string as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_print_i64",
+        ptr: vole_runtime::builtins::vole_print_i64 as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_print_f64",
+        ptr: vole_runtime::builtins::vole_print_f64 as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_print_bool",
+        ptr: vole_runtime::builtins::vole_print_bool as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_print_char",
+        ptr: vole_runtime::builtins::vole_print_char as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_flush",
+        ptr: vole_runtime::builtins::vole_flush as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_i64_to_string",
+        ptr: vole_runtime::builtins::vole_i64_to_string as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_f64_to_string",
+        ptr: vole_runtime::builtins::vole_f64_to_string as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_f32_to_string",
+        ptr: vole_runtime::builtins::vole_f32_to_string as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_i128_to_string",
+        ptr: vole_runtime::builtins::vole_i128_to_string as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_i128_sdiv",
+        ptr: vole_runtime::builtins::vole_i128_sdiv as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_i128_srem",
+        ptr: vole_runtime::builtins::vole_i128_srem as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_bool_to_string",
+        ptr: vole_runtime::builtins::vole_bool_to_string as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_nil_to_string",
+        ptr: vole_runtime::builtins::vole_nil_to_string as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_array_i64_to_string",
+        ptr: vole_runtime::builtins::vole_array_i64_to_string as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_assert_fail",
+        ptr: vole_runtime::assert::vole_assert_fail as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_panic",
+        ptr: vole_runtime::builtins::vole_panic as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_array_new",
+        ptr: vole_runtime::builtins::vole_array_new as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_array_push",
+        ptr: vole_runtime::builtins::vole_array_push as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_array_get_value",
+        ptr: vole_runtime::builtins::vole_array_get_value as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_array_len",
+        ptr: vole_runtime::builtins::vole_array_len as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_array_iter",
+        ptr: vole_runtime::iterator::vole_array_iter as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_interface_iter",
+        ptr: vole_runtime::iterator::vole_interface_iter as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_array_iter_next",
+        ptr: vole_runtime::iterator::vole_array_iter_next as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_array_iter_collect",
+        ptr: vole_runtime::iterator::vole_array_iter_collect as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_map_iter",
+        ptr: vole_runtime::iterator::vole_map_iter as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_map_iter_next",
+        ptr: vole_runtime::iterator::vole_map_iter_next as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_map_iter_collect",
+        ptr: vole_runtime::iterator::vole_map_iter_collect as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_filter_iter",
+        ptr: vole_runtime::iterator::vole_filter_iter as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_filter_iter_next",
+        ptr: vole_runtime::iterator::vole_filter_iter_next as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_filter_iter_collect",
+        ptr: vole_runtime::iterator::vole_filter_iter_collect as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_iter_count",
+        ptr: vole_runtime::iterator::vole_iter_count as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_iter_sum",
+        ptr: vole_runtime::iterator::vole_iter_sum as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_iter_set_elem_tag",
+        ptr: vole_runtime::iterator::vole_iter_set_elem_tag as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_iter_set_produces_owned",
+        ptr: vole_runtime::iterator::vole_iter_set_produces_owned as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_iter_for_each",
+        ptr: vole_runtime::iterator::vole_iter_for_each as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_iter_reduce",
+        ptr: vole_runtime::iterator::vole_iter_reduce as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_iter_reduce_tagged",
+        ptr: vole_runtime::iterator::vole_iter_reduce_tagged as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_iter_first",
+        ptr: vole_runtime::iterator::vole_iter_first as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_iter_last",
+        ptr: vole_runtime::iterator::vole_iter_last as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_iter_nth",
+        ptr: vole_runtime::iterator::vole_iter_nth as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_take_iter",
+        ptr: vole_runtime::iterator::vole_take_iter as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_take_iter_next",
+        ptr: vole_runtime::iterator::vole_take_iter_next as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_take_iter_collect",
+        ptr: vole_runtime::iterator::vole_take_iter_collect as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_skip_iter",
+        ptr: vole_runtime::iterator::vole_skip_iter as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_skip_iter_next",
+        ptr: vole_runtime::iterator::vole_skip_iter_next as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_skip_iter_collect",
+        ptr: vole_runtime::iterator::vole_skip_iter_collect as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_chain_iter",
+        ptr: vole_runtime::iterator::vole_chain_iter as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_chain_iter_next",
+        ptr: vole_runtime::iterator::vole_chain_iter_next as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_chain_iter_collect",
+        ptr: vole_runtime::iterator::vole_chain_iter_collect as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_flatten_iter",
+        ptr: vole_runtime::iterator::vole_flatten_iter as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_flatten_iter_next",
+        ptr: vole_runtime::iterator::vole_flatten_iter_next as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_flatten_iter_collect",
+        ptr: vole_runtime::iterator::vole_flatten_iter_collect as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_flat_map_iter",
+        ptr: vole_runtime::iterator::vole_flat_map_iter as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_flat_map_iter_next",
+        ptr: vole_runtime::iterator::vole_flat_map_iter_next as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_flat_map_iter_collect",
+        ptr: vole_runtime::iterator::vole_flat_map_iter_collect as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_reverse_iter",
+        ptr: vole_runtime::iterator::vole_reverse_iter as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_sorted_iter",
+        ptr: vole_runtime::iterator::vole_sorted_iter as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_unique_iter",
+        ptr: vole_runtime::iterator::vole_unique_iter as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_unique_iter_next",
+        ptr: vole_runtime::iterator::vole_unique_iter_next as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_chunks_iter",
+        ptr: vole_runtime::iterator::vole_chunks_iter as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_chunks_iter_next",
+        ptr: vole_runtime::iterator::vole_chunks_iter_next as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_chunks_iter_collect",
+        ptr: vole_runtime::iterator::vole_chunks_iter_collect as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_windows_iter",
+        ptr: vole_runtime::iterator::vole_windows_iter as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_windows_iter_next",
+        ptr: vole_runtime::iterator::vole_windows_iter_next as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_windows_iter_collect",
+        ptr: vole_runtime::iterator::vole_windows_iter_collect as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_repeat_iter",
+        ptr: vole_runtime::iterator::vole_repeat_iter as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_repeat_iter_next",
+        ptr: vole_runtime::iterator::vole_repeat_iter_next as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_once_iter",
+        ptr: vole_runtime::iterator::vole_once_iter as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_once_iter_next",
+        ptr: vole_runtime::iterator::vole_once_iter_next as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_empty_iter",
+        ptr: vole_runtime::iterator::vole_empty_iter as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_from_fn_iter",
+        ptr: vole_runtime::iterator::vole_from_fn_iter as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_from_fn_iter_next",
+        ptr: vole_runtime::iterator::vole_from_fn_iter_next as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_range_iter",
+        ptr: vole_runtime::iterator::vole_range_iter as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_range_iter_next",
+        ptr: vole_runtime::iterator::vole_range_iter_next as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_string_chars_iter",
+        ptr: vole_runtime::iterator::vole_string_chars_iter as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_string_chars_iter_next",
+        ptr: vole_runtime::iterator::vole_string_chars_iter_next as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_array_set",
+        ptr: vole_runtime::builtins::vole_array_set as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_array_filled",
+        ptr: vole_runtime::builtins::vole_array_filled as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_closure_alloc",
+        ptr: vole_runtime::closure::vole_closure_alloc as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_closure_get_capture",
+        ptr: vole_runtime::closure::vole_closure_get_capture as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_closure_set_capture",
+        ptr: vole_runtime::closure::vole_closure_set_capture as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_closure_set_capture_kind",
+        ptr: vole_runtime::closure::vole_closure_set_capture_kind as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_closure_get_func",
+        ptr: vole_runtime::closure::vole_closure_get_func as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_heap_alloc",
+        ptr: vole_runtime::closure::vole_heap_alloc as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_sb_new",
+        ptr: vole_runtime::string_builder::vole_sb_new as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_sb_push_string",
+        ptr: vole_runtime::string_builder::vole_sb_push_string as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_sb_finish",
+        ptr: vole_runtime::string_builder::vole_sb_finish as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_instance_new",
+        ptr: vole_runtime::instance::vole_instance_new as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_instance_inc",
+        ptr: vole_runtime::instance::vole_instance_inc as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_instance_dec",
+        ptr: vole_runtime::instance::vole_instance_dec as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_instance_get_field",
+        ptr: vole_runtime::instance::vole_instance_get_field as *const u8,
+    },
+    LinkableRuntimeSymbol {
+        c_name: "vole_instance_set_field",
+        ptr: vole_runtime::instance::vole_instance_set_field as *const u8,
+    },
+];
+
+pub fn all_linkable_symbols() -> &'static [LinkableRuntimeSymbol] {
+    LINKABLE_RUNTIME_SYMBOLS
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -488,6 +926,18 @@ mod tests {
                 seen.insert(symbol.key),
                 "duplicate runtime key in registry: {:?}",
                 symbol.key
+            );
+        }
+    }
+
+    #[test]
+    fn linkable_symbol_names_are_unique() {
+        let mut seen = FxHashSet::default();
+        for symbol in all_linkable_symbols() {
+            assert!(
+                seen.insert(symbol.c_name),
+                "duplicate linkable runtime symbol name: {}",
+                symbol.c_name
             );
         }
     }
